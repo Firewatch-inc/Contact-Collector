99a5b85b8b94e45d1f0b6a2bed7595c2e711d160
Sergey <pmswga@gmail.com>
2018-04-22 22:32:33 +0300

    Обновлены настройки соединения к базе данных

--- engine/settings.php
+++ engine/settings.php
@@ -1,8 +1,8 @@
 <?php
 	
 	const LOCAL_HOST = "localhost";
-	const USER_NAME = "root";
-	const USER_PASSWORD = "57RbhCjy";
+	const USER_NAME = "peop";
+	const USER_PASSWORD = "peop_pass";
 	const DATA_BASE_NAME = "peops";
 	const PORT = 3306;
 	
