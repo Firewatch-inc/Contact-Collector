fbbb43680301bd8ae70de5ecbc2a0ef1452c424b
Sergey <pmswga@gmail.com>
2018-06-25 23:55:06 +0300

    Удаление всех исходников для разработки с нуля

--- engine/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	const LOCAL_HOST = "localhost";
-	const USER_NAME = "peop";
-	const USER_PASSWORD = "peop_pass";
-	const DATA_BASE_NAME = "peops";
-	const PORT = 3306;
-	
-?>
--- sql/users.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE USER `peop`@`localhost`;
-
-GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, REFERENCES ON peops.* TO `peop`@`localhost`;
-
-SET PASSWORD FOR 'peop'@'localhost' = PASSWORD('peop_pass');
